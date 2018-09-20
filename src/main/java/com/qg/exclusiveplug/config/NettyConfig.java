776f81c269fd44dd9e28cc787280c85381191b43
WilderGao <845956752@qq.com>
2018-09-20 21:48:18 +0800

    先配置Druid 和 Netty

--- src/main/java/com/qg/exclusiveplug/config/NettyConfig.java
+++ src/main/java/com/qg/exclusiveplug/config/NettyConfig.java
@@ -7,7 +7,6 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
-import org.apache.catalina.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
